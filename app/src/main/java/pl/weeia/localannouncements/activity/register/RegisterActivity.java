1decf6592aec4c44e062d558f80c6830982c1a10
pszczolkowski <pszczolkowski.k@gmail.com>
2016-10-29 11:37:10 +0200

    Refactor register activity code and layout

--- app/src/main/java/pl/weeia/localannouncements/activity/register/RegisterActivity.java
+++ app/src/main/java/pl/weeia/localannouncements/activity/register/RegisterActivity.java
@@ -1,248 +1,128 @@
 package pl.weeia.localannouncements.activity.register;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pl.weeia.localannouncements.R;
 import pl.weeia.localannouncements.activity.main.MainActivity;
 import pl.weeia.localannouncements.api.Failure;
-import pl.weeia.localannouncements.api.user.AuthenticationFailure;
 import pl.weeia.localannouncements.api.user.RegistrationFailure;
 import pl.weeia.localannouncements.api.user.RegistrationForm;
 import pl.weeia.localannouncements.api.user.UserApi;
-import pl.weeia.localannouncements.service.user.ApplicationUser;
-import pl.weeia.localannouncements.service.user.Authenticator;
 import pl.weeia.localannouncements.shared.AndroidUiDoneCallback;
 import pl.weeia.localannouncements.shared.AndroidUiFailCallback;
 import pl.weeia.localannouncements.shared.BaseActivity;
 import pl.weeia.localannouncements.shared.enums.Gender;
 
-import static pl.weeia.localannouncements.api.user.AuthenticationFailure.BAD_CREDENTIALS;
+import static android.view.View.VISIBLE;
 import static pl.weeia.localannouncements.api.user.RegistrationFailure.EMAIL_ALREADY_TAKEN;
 import static pl.weeia.localannouncements.api.user.RegistrationFailure.USERNAME_ALREADY_TAKEN;
+import static pl.weeia.localannouncements.shared.enums.Gender.FEMALE;
 import static pl.weeia.localannouncements.shared.enums.Gender.MALE;
 
 public class RegisterActivity extends BaseActivity {
 
-
     @Inject
     protected UserApi userApi;
 
-    @Inject
-    protected Authenticator authenticator;
-
-    private RadioGroup radioGroup;
-    private RadioButton radioGender;
-    private TextView register_err;
-    private EditText loginText, passwordText, password_repeatText, ageText, emailText;
-    private Spinner genderText;
-
-    private String login, password, password_repeat, ageTxt, email;
-    private Gender register_gender;
-
-    //email pattern
-    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-
-    //orginal backgroundresource
-    Drawable originalDrawable;
+    @BindView(R.id.register_edittext_username)
+    protected EditText usernameEditText;
+    @BindView(R.id.register_edittext_password)
+    protected EditText passwordEditText;
+    @BindView(R.id.register_edittext_email)
+    protected EditText emailEditText;
+    @BindView(R.id.register_edittext_age)
+    protected EditText ageEditText;
+    @BindView(R.id.register_radiogroup_gender)
+    protected RadioGroup genderRadioGroup;
+    @BindView(R.id.register_textview_error)
+    protected TextView errorTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        // These two lines are required for injection
-        // They need to be copied for every Activity
+
         ButterKnife.bind(this);
         getContainer().inject(this);
+    }
 
+    @OnClick(R.id.register_button_sign_up)
+    public void onSignUpButtonClick(Button button) {
+        RegistrationForm registrationForm = generateRegistrationForm();
 
-        Button register = (Button)findViewById(R.id.buttonRegister);
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
-        register_err = (TextView)findViewById(R.id.register_error);
-        loginText = (EditText)findViewById(R.id.log_in);
-        passwordText = (EditText)findViewById(R.id.password);
-        password_repeatText = (EditText)findViewById(R.id.password_repeat);
-        ageText = (EditText)findViewById(R.id.age);
-        emailText = (EditText)findViewById(R.id.e_mail);
-        //genderText = (Spinner)findViewById(R.id.gender);
-        originalDrawable = loginText.getBackground();
+        usernameEditText.setError("blablabab");
 
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                boolean wrong_fill;
-                int radio_index = radioGroup.getCheckedRadioButtonId();
-                radioGender = (RadioButton)findViewById(radio_index);
-                login = loginText.getText().toString();
-                password = passwordText.getText().toString();
-                password_repeat = password_repeatText.getText().toString();
-                ageTxt = ageText.getText().toString();
-                email = emailText.getText().toString();
-
-                //set default view
-                EditTextDefault();
-                //check validation
-                wrong_fill = Validation();
-                if(wrong_fill == true)
-                {
-
+        userApi
+            .register(registrationForm)
+            .then(new AndroidUiDoneCallback<Void>() {
+                @Override
+                public void onDone(Void dummy) {
+                    startMainActivity();
                 }
-                else
-                {
-
-                    if(radioGender.getText().toString().equals("Male"))
-                    {
-                        register_gender = Gender.MALE;
-                    }
-                    else
-                    {
-                        register_gender = Gender.FEMALE;
-                    }
-
-                    int age = Integer.parseInt(ageTxt);
-
-
-                    RegistrationForm registrationForm = new RegistrationForm()
-                            .withUsername(login)
-                            .withPassword(password)
-                            .withAge(age)
-                            .withGender(register_gender)
-                            .withEmail(email);
-
-
-                    userApi
-                            .register(registrationForm)
-                            .then(new AndroidUiDoneCallback<Void>() {
-                                @Override
-                                public void onDone(Void dummy) {
-                                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                                    startActivity(i);
-                                    finish();
-                                }
-                            }).fail(new AndroidUiFailCallback<Failure<RegistrationFailure>>() {
-                        @Override
-                        public void onFail(Failure<RegistrationFailure> failure) {
-                            if (failure.isBecauseOfThrowable()) {
-                                // TODO some error occured, maybe there;s no internet connection?
-                                register_err.setText("Connect Error");
-                                failure.getThrowable().printStackTrace();
-                            } else if (failure.isBecauseOf(USERNAME_ALREADY_TAKEN)) {
-                                // TODO that username is alredy used by some other dude
-                                register_err.setText("User exist");
-                            } else if (failure.isBecauseOf(EMAIL_ALREADY_TAKEN)) {
-                                // TODO that email address is alredy used by some other dude
-                                register_err.setText("E-mail is already use");
-                            }
-                        }
-                    });
-
+            }).fail(new AndroidUiFailCallback<Failure<RegistrationFailure>>() {
+                @Override
+                public void onFail(Failure<RegistrationFailure> failure) {
+                    onRegistrationFailure(failure);
                 }
+            });
+    }
 
+    private RegistrationForm generateRegistrationForm() {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        int age = Integer.parseInt(ageEditText.getText().toString());
+        int checkedGenderId = genderRadioGroup.getCheckedRadioButtonId();
+        Gender gender = checkedGenderId == R.id.register_radiobutton_male ? MALE : FEMALE;
 
+        return new RegistrationForm()
+                .withUsername(username)
+                .withPassword(password)
+                .withAge(age)
+                .withGender(gender)
+                .withEmail(email);
+    }
 
+    private void startMainActivity() {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
+    }
 
+    private void onRegistrationFailure(Failure<RegistrationFailure> failure) {
+        if (failure.isBecauseOf(USERNAME_ALREADY_TAKEN)) {
+            showRegistrationError(R.string.register_message_username_taken);
+        } else if (failure.isBecauseOf(EMAIL_ALREADY_TAKEN)) {
+            showRegistrationError(R.string.register_message_email_taken);
+        } else {
+            showErrorMessage();
 
-
-
-
-
-
-
-                //end onClick
+            if (failure.isBecauseOfThrowable()) {
+                failure.getThrowable().printStackTrace();
             }
-        });
-
+        }
     }
 
-
-    void EditTextDefault()
-    {
-        loginText.setBackground(originalDrawable);
-        passwordText.setBackground(originalDrawable);
-        password_repeatText.setBackground(originalDrawable);
-        ageText.setBackground(originalDrawable);
-        emailText.setBackground(originalDrawable);
-        register_err.setText("");
+    private void showRegistrationError(int resourceId) {
+        errorTextView.setText(getResources().getString(resourceId));
+        errorTextView.setVisibility(VISIBLE);
     }
 
-    boolean Validation()
-    {
-        boolean wrong_fill = false;
-
-        if(login.trim().length() == 0)
-        {
-            register_err.setText("You have empty spaces");
-            loginText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(login.trim().length() < 5)
-        {
-            register_err.setText("Login too small");
-            loginText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(password.trim().length() == 0)
-        {
-            register_err.setText("You have empty spaces");
-            passwordText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-
-        if(password.trim().length() < 8)
-        {
-            register_err.setText("Password too small");
-            passwordText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-
-        if(password_repeat.trim().length() == 0)
-        {
-            register_err.setText("You have empty spaces");
-            password_repeatText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(email.trim().length() == 0)
-        {
-            register_err.setText("You have empty spaces");
-            emailText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(email.trim().length() == 0)
-        {
-            register_err.setText("You have empty spaces");
-            //ageText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(password.equals(password_repeat) == false)
-        {
-            register_err.setText("Wrong password");
-            password_repeatText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-        if(email.matches(emailPattern) == false)
-        {
-            register_err.setText("Wrong e-mail adress");
-            emailText.setBackgroundResource(R.drawable.edittext_bg);
-            wrong_fill = true;
-        }
-
-
-        return wrong_fill;
+    private void showErrorMessage() {
+        Toast
+            .makeText(RegisterActivity.this, R.string.register_message_error, Toast.LENGTH_SHORT)
+            .show();
     }
 
 }
